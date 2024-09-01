2a2dfa6bb941fdcee36ae84193f820d7b0246a61
ashbur <18829526908@163.com>
2024-09-01 17:05:25 +0800

    第一节：第一个 SpringSecurity 程序

---
+++ first-security/src/main/java/com/rookie/stack/security/Application.java
@@ -0,0 +1,17 @@
+package com.rookie.stack.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author eumenides
+ * @description
+ * @date 2024/9/1
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
